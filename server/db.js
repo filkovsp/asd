5f093defcd05c79380dc7c0aa486303f720f8061
Pavel <pavel.filkovskiy@gmail.com>
2022-01-16 17:24:29 +0000

    rever back db connectionString

--- server/db.js
+++ server/db.js
@@ -3,15 +3,19 @@ import { Pool } from "pg";
 const dbUrl = process.env.DATABASE_URL || "postgres://localhost:5432/asd";
 
 export const pool = new Pool({
-	// connectionString: dbUrl,
-	host: "localhost",
-	port: 5432,
-	database: "asd",
-	user: "postgres",
-	password: "postgres",
+	connectionString: dbUrl,
 	connectionTimeoutMillis: 5000,
 	ssl: dbUrl.includes("localhost") ? false : { rejectUnauthorized: false },
 });
+
+/**
+ * above `connectionString` will require system user being able to access the `asd` db
+ * if not allowed, then you might see error:
+ * 	Error: role "Pavel" does not exist
+ * troubleshooting:
+ * loging to psql and connect to the asd db, the run below script:
+ * create user "Pavel" with login;
+ */
 
 export const connectDb = async () => {
 	let client;
